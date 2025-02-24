10962e715fb89c3a432679225b06dba674ee04b4
Stuart Wheater <stuart.wheater@arjuna.com>
2025-02-24 10:21:41 +0000

    Initial version of 'ds-upload-pipeline-blockly'

---
+++ ds-upload-pipeline-blockly/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BlocklyView from './components/BlocklyView.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <BlocklyView/>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header
+{
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
---
+++ ds-upload-pipeline-blockly/src/components/BlocklyView.vue
@@ -0,0 +1,264 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import * as Blockly from 'blockly/core';
+</script>
+
+<template>
+  <div class="greetings">
+    <div id="blocklyDiv" ref="blocklyDiv" style="height: 720px; width: 1024px;"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default
+  {
+    mounted()
+    {
+      console.log('Before Block');
+      // LoaderData
+      // WriteTable
+      // Summary
+      // Validater
+      // FromLongLong
+      // ArmadilloUploader
+
+      Blockly.Blocks['upload_pipeline'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Upload Pipeline');
+          this.appendStatementInput('Steps')
+              .setCheck('STEP')
+              .appendField('steps');
+          this.setPreviousStatement(false);
+          this.setOutput(false);
+          this.setNextStatement(false);
+          this.setColour(45);
+          this.setTooltip('Upload Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['loader_data'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Load Data');
+          this.appendValueInput('Filename')
+              .setCheck('String')
+              .appendField('filename');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(90);
+          this.setTooltip('Load Data: Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['write_table'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Write Table');
+          this.appendValueInput('Filename')
+              .setCheck('String')
+              .appendField('filename');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(135);
+          this.setTooltip('Write Table: Pipeline nodes');
+        }
+      };
+
+      Blockly.Blocks['summary'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Summary');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(180);
+          this.setTooltip('Summary: Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['validater'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Validator');
+          this.appendStatementInput('ColumnSpecs')
+              .setCheck('COLUMNSPEC')
+              .appendField('column specs');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(225);
+          this.setTooltip('Validator: Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['from_long_long'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('From Long Long to Wide');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(270);
+          this.setTooltip('From Long Long to Wide: Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['armadillo_uploader'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Armadillo Uploader');
+          this.appendValueInput('ArmadilloURL')
+              .setCheck('String')
+              .appendField('armdillo url');
+          this.appendValueInput('Username')
+              .setCheck('String')
+              .appendField('user name');
+          this.appendValueInput('password')
+              .setCheck('String')
+              .appendField('password');
+          this.appendValueInput('ProjectName')
+              .setCheck('String')
+              .appendField('project name');
+          this.appendValueInput('FolderName')
+              .setCheck('String')
+              .appendField('folder name');
+          this.appendValueInput('TableName')
+              .setCheck('String')
+              .appendField('table name');
+          this.setPreviousStatement(true, "STEP");
+          this.setOutput(false);
+          this.setNextStatement(true, "STEP");
+          this.setColour(315);
+          this.setTooltip('Armadillo Uploader: Pipeline node');
+        }
+      };
+
+      Blockly.Blocks['column_spec'] =
+      {
+        init: function()
+        {
+          this.appendDummyInput()
+              .appendField('Column Spec');
+          this.appendValueInput('ColumnNameColumnName')
+              .setCheck('String')
+              .appendField('column name\'s column name');
+          this.appendValueInput('RowNameColumnName')
+              .setCheck('String')
+              .appendField('row name\'s column name');
+          this.appendValueInput('ValueColumnName')
+              .setCheck('String')
+              .appendField('value column\'s name');
+          this.setPreviousStatement(true, "COLUMNSPEC");
+          this.setOutput(false);
+          this.setNextStatement(true, "COLUMNSPEC");
+          this.setColour(270);
+          this.setTooltip('Column Spec: Validation node');
+        }
+      };
+
+      const workspace = Blockly.inject(this.$refs.blocklyDiv, {toolbox: 
+        {
+          "kind": 'categoryToolbox',
+          "contents":
+          [
+            {
+              "kind": "category",
+              "name": "Structures",
+              "contents":
+              [
+                {
+                  "kind": "block",
+                  "type": "upload_pipeline"
+                }
+              ]
+            },
+            {
+              "kind": "category",
+              "name": "Steps",
+              "contents":
+              [
+                {
+                  "kind": "block",
+                  "type": "loader_data"
+                },
+                {
+                  "kind": "block",
+                  "type": "write_table"
+                },
+                {
+                  "kind": "block",
+                  "type": "summary"
+                },
+                {
+                  "kind": "block",
+                  "type": "validater"
+                },
+                {
+                  "kind": "block",
+                  "type": "from_long_long"
+                },
+                {
+                  "kind": "block",
+                  "type": "armadillo_uploader"
+                }
+              ]
+            },
+            {
+              "kind": "category",
+              "name": "Checks",
+              "contents":
+              [
+                {
+                  "kind": "block",
+                  "type": "column_spec"
+                }
+              ]
+            },
+            {
+              "kind": "category",
+              "name": "Values",
+              "contents":
+              [
+                {
+                  "kind": "block",
+                  "type": "math_number"
+                }
+              ]
+            }
+          ]
+        }
+      });
+    }
+  }
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+</style>
